29ea587b1a98ab6bdf25e62ab4f4b4d3889b03a7
Richard Caballero <richc117@gmail.com>
2022-09-29 22:20:51 -0700

    Delete classex.py

--- classex.py
+++
@@ -1,8 +0,0 @@
-multi_table = [['X' if i==j==0 else max(i,j) if i*j == 0 else i*j for i in range(0,13)] for j in range(0,13)]
-
-def test_multi_table_case():
-    print()
-    assert multi_table[0][0] == 'X'
-    assert multi_table[2][0] == 2
-    assert multi_table[0][2] == 2
-    assert multi_table[6][6] == 36
